52c4b9a41ac1e72186861f037918c10ee69e3ae8
suvieu <42606502+suvieu@users.noreply.github.com>
2019-11-19 22:38:30 +0800

    Update tosql.py

--- SCRAPING/MTIME/tosql.py
+++ SCRAPING/MTIME/tosql.py
@@ -1,6 +1,6 @@
 import pymysql
 def Inser(VALUE):
-    db = pymysql.connect(host="localhost", user="Simon",password= "narutoONEPIECE7",port=3306, db='movie')
+    db = pymysql.connect(host="localhost", user="Simon",password= "*****",port=3306, db='movie')
     cursor = db.cursor()
     sql = "INSERT INTO MTIME(NAME,LINK,POINT,POINT2,YEAR) VALUES (%s,%s,%s,%s,%s)"
     cursor.execute(sql,VALUE)
